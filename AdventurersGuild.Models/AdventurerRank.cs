6e86416aef5c5a81ef676e77782ff2bbb1e65117
goddamngh <saveliishiriev@gmail.com>
2026-05-06 02:16:04 +0700

    Удалены bin/ и obj/ из отслеживания Git

--- AdventurersGuild.Models/AdventurerRank.cs
+++ AdventurersGuild.Models/AdventurerRank.cs
@@ -1,12 +1,20 @@
 namespace AdventurersGuild.Models;
+using System.ComponentModel;
 
 public enum AdventurerRank
 {
+    [Description("F")]
     F,
+    [Description("E")]
     E,
+    [Description("D")]
     D,
+    [Description("C")]
     C,
+    [Description("B")]
     B,
+    [Description("A")]
     A,
+    [Description("S")]
     S
 }
