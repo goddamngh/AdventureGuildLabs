e232c557a383df9bc9aff1ae0b5006df44d13c7f
goddamngh <saveliishiriev@gmail.com>
2026-05-06 05:07:26 +0700

    Добавлена миграция AddQuestAdventurerRelation
    - Создание внешнего ключа AdventurerId в таблице Quests
    - Связь между квестами и авантюристами
    - Обновлён ModelSnapshot

---
+++ AdventurersGuild.Database/Migrations/20260505212145_AddQuestAdventurerRelation.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdventurersGuild.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuestAdventurerRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AdventurerId",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "AdventurerId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_AdventurerId",
+                table: "Questions",
+                column: "AdventurerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Adventurers_AdventurerId",
+                table: "Questions",
+                column: "AdventurerId",
+                principalTable: "Adventurers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Adventurers_AdventurerId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_AdventurerId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "AdventurerId",
+                table: "Questions");
+        }
+    }
+}
